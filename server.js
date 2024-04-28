a80f9347b50e87f93f73a81bda88b288843b7aac
Carlos Carvajal <carv0066@algonquinlive.com>
2024-04-28 00:41:45 -0400

    Working on register

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors());
 
 app.get('/', (req, res) => { res.send('success') })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
