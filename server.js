db694181ac457bf1aa8f41303568674bbed346e7
Carlos Carvajal <carv0066@algonquinlive.com>
2024-05-04 22:34:51 -0400

    Made some changes to the imports and the app.requests

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ import bcrypt from 'bcrypt-nodejs';
 import cors from 'cors';
 import knex from 'knex';
 
-import register from "./controllers/register.js";
-import signin from "./controllers/signin.js";
-import profile from "./controllers/profile.js";
-import image from "./controllers/image.js";
+import handleRegister from "./controllers/register.js";
+import handleSignin from "./controllers/signin.js";
+import handleProfile from "./controllers/profile.js";
+import { handleImage, handleApiCall } from "./controllers/image.js";
 
 const db = knex({
     client: 'pg',
@@ -27,12 +27,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+//Made some changes to the imports 
 app.get('/', (req, res) => { res.send('success') })
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+app.post('/signin', (req, res) => { handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { handleProfile(req, res, db) })
+app.put('/image', (req, res) => { handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { handleApiCall(req, res) })
 app.listen(process.env.PORT || 3000, () => {
     console.log(`App is running on port ${process.env}`);
 })
