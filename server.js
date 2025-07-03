b22b9f1c1da07995ab746c914eb391af11b5034b
Carlos Carvajal <carv0066@algonquinlive.com>
2025-07-02 22:41:41 -0400

    Final Commit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-//Made some changes to the imports 
+//Made some changes to the imports.
 app.get('/', (req, res) => { res.send('success') })
 app.post('/signin', (req, res) => { handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) })
